6015f904aac1336579d523e470df2a0574b7e7fa
Justus <justus@justcoding.tech>
2024-09-20 13:47:56 +0200

    New translations general.php (VCP) Fixed: New translations general.php from Crowdin

--- resources/lang/de-VCP/admin/asset_maintenances/general.php
+++ resources/lang/de-VCP/admin/asset_maintenances/general.php
@@ -1,16 +1,16 @@
 <?php
 
     return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
+        'asset_maintenances' => 'Asset Wartungen',
+        'edit'               => 'Wartung bearbeiten',
+        'delete'             => 'Wartung löschen',
+        'view'               => 'Wartungsdetails anzeigen',
+        'repair'             => 'Reparatur',
+        'maintenance'        => 'Wartung',
         'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
+        'calibration'        => 'Kalibrierung',
         'software_support'   => 'Software Support',
         'hardware_support'   => 'Hardware Support',
-        'configuration_change'   => 'Configuration Change',
-        'pat_test'           => 'PAT Test',
+        'configuration_change'   => 'Änderungen an der Konfiguration',
+        'pat_test'           => 'DGUV-Prüfung',
     ];
