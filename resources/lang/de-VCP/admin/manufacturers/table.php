2d0049d2dccf98314b651c5660a61858d9ed3500
Justus <justus@justcoding.tech>
2024-09-20 13:48:36 +0200

    New translations table.php (VCP) Fixed: New translations table.php from Crowdin

--- resources/lang/de-VCP/admin/manufacturers/table.php
+++ resources/lang/de-VCP/admin/manufacturers/table.php
@@ -1,16 +1,16 @@
 <?php
 
 return array(
-    'about_manufacturers_title'    => 'About manufacturers',
-    'about_manufacturers_text'  => 'Manufacturers are the companies that create your assets. You can store important support contact information about them here, which will be displayed on your asset detail pages.',
-    'asset_manufacturers'	=> 'Asset Manufacturers',
-    'create'				=> 'Create Manufacturer',
+    'about_manufacturers_title'    => 'Über Hersteller',
+    'about_manufacturers_text'  => 'Hersteller sind Firmen, die ihre Assets herstellen. Hier können Sie wichtige Supportkontakte eintragen, welche in den Assetdetails angezeigt werden.',
+    'asset_manufacturers'	=> 'Asset Hersteller',
+    'create'				=> 'Hersteller anlegen',
     'id'   					=> 'ID',
     'name'      			=> 'Name',
-    'support_email'   		=> 'Support Email',
-    'support_phone'   		=> 'Support Phone',
-    'support_url'   		=> 'Support URL',
-    'warranty_lookup_url'   => 'Warranty Lookup URL',
-    'update'				=> 'Update Manufacturer',
+    'support_email'   		=> 'Support per E-Mail',
+    'support_phone'   		=> 'Support Telefon',
+    'support_url'   		=> 'Support-URL',
+    'warranty_lookup_url'   => 'Link zu Garantiebedingungen',
+    'update'				=> 'Hersteller aktualisieren',
 
 );
