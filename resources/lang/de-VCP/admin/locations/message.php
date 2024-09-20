b377a80bd780d5d84684ca6f666e96ba55a7f081
Justus <justus@justcoding.tech>
2024-09-20 13:48:33 +0200

    New translations message.php (VCP) Fixed: New translations message.php from Crowdin

--- resources/lang/de-VCP/admin/locations/message.php
+++ resources/lang/de-VCP/admin/locations/message.php
@@ -2,34 +2,34 @@
 
 return array(
 
-    'does_not_exist' => 'Location does not exist.',
-    'assoc_users'    => 'This location is not currently deletable because it is the location of record for at least one asset or user, has assets assigned to it, or is the parent location of another location. Please update your records to no longer reference this location and try again. ',
-    'assoc_assets'	 => 'This location is currently associated with at least one asset and cannot be deleted. Please update your assets to no longer reference this location and try again. ',
-    'assoc_child_loc'	 => 'This location is currently the parent of at least one child location and cannot be deleted. Please update your locations to no longer reference this location and try again. ',
-    'assigned_assets' => 'Assigned Assets',
-    'current_location' => 'Current Location',
-    'open_map' => 'Open in :map_provider_icon Maps',
+    'does_not_exist' => 'Standort nicht verfügbar.',
+    'assoc_users'    => 'Dieser Standort kann derzeit nicht gelöscht werden, da er der Standort eines Datensatzes für mindestens ein Asset oder einen Benutzer ist, ihm Assets zugewiesen sind oder er der übergeordnete Standort eines anderen Standorts ist. Aktualisieren Sie Ihre Datensätze, sodass dieser Standort nicht mehr referenziert wird, und versuchen Sie es erneut. ',
+    'assoc_assets'	 => 'Dieser Standort ist aktuell mindestens einem Gegenstand zugewiesen und kann nicht gelöscht werden. Bitte entfernen Sie die Standortzuweisung bei den jeweiligen Gegenständen und versuchen Sie es erneut diesen Standort zu entfernen. ',
+    'assoc_child_loc'	 => 'Dieser Ort ist aktuell mindestens einem anderen Ort übergeordnet und kann nicht gelöscht werden. Bitte Orte aktualisieren, so dass dieser Standort nicht mehr verknüpft ist und erneut versuchen. ',
+    'assigned_assets' => 'Zugeordnete Assets',
+    'current_location' => 'Aktueller Standort',
+    'open_map' => 'Öffnen in :map_provider_icon Karten',
 
 
     'create' => array(
-        'error'   => 'Location was not created, please try again.',
-        'success' => 'Location created successfully.'
+        'error'   => 'Standort wurde nicht erstellt, bitte versuchen Sie es erneut.',
+        'success' => 'Standort erfolgreich erstellt.'
     ),
 
     'update' => array(
-        'error'   => 'Location was not updated, please try again',
-        'success' => 'Location updated successfully.'
+        'error'   => 'Standort wurde nicht aktualisiert, bitte erneut versuchen',
+        'success' => 'Standort erfolgreich aktualisiert.'
     ),
 
     'restore' => array(
-        'error'   => 'Location was not restored, please try again',
-        'success' => 'Location restored successfully.'
+        'error'   => 'Der Standort wurde nicht wiederhergestellt. Bitte versuchen Sie es erneut',
+        'success' => 'Standort erfolgreich wiederhergestellt.'
     ),
 
     'delete' => array(
-        'confirm'   	=> 'Are you sure you wish to delete this location?',
-        'error'   => 'There was an issue deleting the location. Please try again.',
-        'success' => 'The location was deleted successfully.'
+        'confirm'   	=> 'Möchten Sie diesen Standort wirklich entfernen?',
+        'error'   => 'Es gab einen Fehler beim Löschen des Standorts. Bitte erneut versuchen.',
+        'success' => 'Der Standort wurde erfolgreich gelöscht.'
     )
 
 );
