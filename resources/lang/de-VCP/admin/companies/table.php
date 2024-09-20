b62362eccc9a1873d213b30561c264b658985da4
Justus <justus@justcoding.tech>
2024-09-20 13:48:04 +0200

    New translations table.php (VCP) Fixed: New translations table.php from Crowdin

--- resources/lang/de-VCP/admin/companies/table.php
+++ resources/lang/de-VCP/admin/companies/table.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'companies' => 'Companies',
-    'create'    => 'Create Company',
-    'email'     => 'Company Email',
-    'title'     => 'Company',
-    'phone'     => 'Company Phone',
-    'update'    => 'Update Company',
-    'name'      => 'Company Name',
+    'companies' => 'Firmen',
+    'create'    => 'Firma erstellen',
+    'email'     => 'Firma E-Mail',
+    'title'     => 'Firma',
+    'phone'     => 'Firmentelefon',
+    'update'    => 'Firma aktualisieren',
+    'name'      => 'Firmenname',
     'id'        => 'ID',
 );
