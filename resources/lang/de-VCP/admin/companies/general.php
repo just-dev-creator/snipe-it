348e78e70193d230b274e8852ec6d359cb86348b
Justus <justus@justcoding.tech>
2024-09-20 13:48:02 +0200

    New translations general.php (VCP) Fixed: New translations general.php from Crowdin

--- resources/lang/de-VCP/admin/companies/general.php
+++ resources/lang/de-VCP/admin/companies/general.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'select_company' => 'Select Company',
-    'about_companies' => 'About Companies',
-    'about_companies_description' => '        You can use companies as a simple informative field, or you can use them to restrict asset visibility and availability to users with a specific company by enabling Full Company Support in your Admin Settings.',
+    'select_company' => 'Firma auswählen',
+    'about_companies' => 'Über Unternehmen',
+    'about_companies_description' => '        Unternehmen können als ein einfaches Informationsfeld verwendet werden, oder um die Sichtbarkeit und Verfügbarkeit von Assets auf Benutzer eines bestimmten Unternehmens zu beschränken. Hierfür muss die Mehrmandanten-Unterstützung für Firmen in den Admin-Einstellungen aktiviert werden.',
 ];
