207a5643f27a5eb5221ba60fdbdf29ab6dc3fdb6
Justus <justus@justcoding.tech>
2024-09-20 13:48:35 +0200

    New translations message.php (VCP) Fixed: New translations message.php from Crowdin

--- resources/lang/de-VCP/admin/manufacturers/message.php
+++ resources/lang/de-VCP/admin/manufacturers/message.php
@@ -2,29 +2,29 @@
 
 return array(
 
-    'support_url_help' => 'Variables <code>{LOCALE}</code>, <code>{SERIAL}</code>, <code>{MODEL_NUMBER}</code>, and <code>{MODEL_NAME}</code> may be used in your URL to have those values auto-populate when viewing assets - for example https://checkcoverage.apple.com/{LOCALE}/{SERIAL}.',
-    'does_not_exist' => 'Manufacturer does not exist.',
-    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
+    'support_url_help' => 'Variablen <code>{LOCALE}</code>, <code>{SERIAL}</code>, <code>{MODEL_NUMBER}</code>, und <code>{MODEL_NAME}</code> kann in Ihrer URL verwendet werden, um diese Werte automatisch zu füllen, wenn Sie Assets sehen - zum Beispiel https://checkcoverage. pple.com/{LOCALE}/{SERIAL}.',
+    'does_not_exist' => 'Hersteller existiert nicht.',
+    'assoc_users'	 => 'Dieser Hersteller ist bereits mit einem Model verknüpft und kann nicht gelöscht werden. Bitte trennen sie Ihre Modelle von diesem Hersteller und versuchen Sie es Erneut.',
 
     'create' => array(
-        'error'   => 'Manufacturer was not created, please try again.',
-        'success' => 'Manufacturer created successfully.'
+        'error'   => 'Der Hersteller konnte nicht erstellt werden, bitte versuchen sie es erneut.',
+        'success' => 'Der Hersteller wurde erfolgreich erstellt.'
     ),
 
     'update' => array(
-        'error'   => 'Manufacturer was not updated, please try again',
-        'success' => 'Manufacturer updated successfully.'
+        'error'   => 'Der Hersteller konnte nicht aktualisiert werden, bitte versuchen sie es erneut',
+        'success' => 'Der Hersteller wurde erfolgreich erstellt.'
     ),
 
     'restore' => array(
-        'error'   => 'Manufacturer was not restored, please try again',
-        'success' => 'Manufacturer restored successfully.'
+        'error'   => 'Der Hersteller wurde nicht wiederhergestellt. Bitte versuchen Sie es erneut',
+        'success' => 'Hersteller wurde erfolgreich wiederhergestellt.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
-        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
-        'success' => 'The Manufacturer was deleted successfully.'
+        'confirm'   => 'Sind sie sicher, dass sie diesen Hersteller löschen wollen?',
+        'error'   => 'Beim löschen des Herstellers ist ein Problem aufgetreten. Bitte versuchen sie es erneut.',
+        'success' => 'Der Hersteller wurde erfolgreich gelöscht.'
     )
 
 );
