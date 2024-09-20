7876681f79d618215728058c76728392a7633df3
Justus <justus@justcoding.tech>
2024-09-20 13:48:15 +0200

    New translations general.php (VCP) Fixed: New translations general.php from Crowdin

--- resources/lang/de-VCP/admin/depreciations/general.php
+++ resources/lang/de-VCP/admin/depreciations/general.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'about_asset_depreciations'  			=> 'About Asset Depreciations',
-    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
-    'asset_depreciations'  					=> 'Asset Depreciations',
-    'create'  					            => 'Create Depreciation',
-    'depreciation_name'  					=> 'Depreciation Name',
-    'depreciation_min'                      => 'Floor Value of Depreciation',
-    'number_of_months'  					=> 'Number of Months',
-    'update'  					            => 'Update Depreciation',
-    'depreciation_min'                      => 'Minimum Value after Depreciation',
-    'no_depreciations_warning'               => '<strong>Warning: </strong>
-                      You do not currently have any depreciations set up.
-                      Please set up at least one depreciation to view the depreciation report.',
+    'about_asset_depreciations'  			=> 'Über Asset-Abschreibungen',
+    'about_depreciations'  					=> 'Sie können Asset-Abschreibungen einrichten, um Assets linear abzuschreiben.',
+    'asset_depreciations'  					=> 'Asset-Abschreibungen',
+    'create'  					            => 'Abschreibung erstellen',
+    'depreciation_name'  					=> 'Abschreibungs Name',
+    'depreciation_min'                      => 'Abschreibungsuntergrenze',
+    'number_of_months'  					=> 'Anzahl der Monate',
+    'update'  					            => 'Abschreibung aktualisieren',
+    'depreciation_min'                      => 'Minimaler Wert nach Abschreibung',
+    'no_depreciations_warning'               => '<strong>Warnung: </strong>
+                      Sie haben derzeit keine Abschreibungen eingerichtet.
+                      Bitte richten Sie mindestens eine Abschreibung ein, um den Abschreibungsbericht anzuzeigen.',
 ];
